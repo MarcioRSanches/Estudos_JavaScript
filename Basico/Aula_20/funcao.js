6a955869cb632cda4f9f5b7748b62dee8dff1be2
Marcio R Sanches <marciosanches@outlook.com>
2020-06-19 15:32:51 -0300

    funcao js

--- Basico/Aula_20/funcao.js
+++ Basico/Aula_20/funcao.js
@@ -8,3 +8,24 @@ function parimp(n) {
 
 console.log(parimp(10))
 console.log(parimp(7))
+
+
+function soma(n1, n2) {
+    console.log(n1 + n2)
+}
+
+soma(10, 5)
+
+function result_Soma(n1, n2) {
+    var resultado = n1 + n2
+    return resultado
+}
+
+console.log(result_Soma(15,25))
+
+// simplificado
+function result_Soma(n1, n2) {
+    return n1 + n2
+}
+
+console.log(result_Soma(15,25))
